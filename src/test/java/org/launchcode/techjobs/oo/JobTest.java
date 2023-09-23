0d2ba73732b8936bb8e528ab282c6fff841f3f0c
Ekaterina <katekhusainova8@gmail.com>
2023-09-23 15:14:02 -0400

    Finished Task 4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,42 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class JobTest {
+
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId () {
+       Job test_job1 = new Job();
+       Job test_job2 = new Job();
+       assertNotEquals(test_job1.getId(), test_job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields () {
+        Job test_job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals("Product tester", test_job3.getName());
+        assertEquals("ACME", test_job3.getEmployer().getValue());
+        assertEquals("Desert", test_job3.getLocation().getValue());
+        assertEquals("Quality control", test_job3.getPositionType().getValue());
+        assertEquals("Persistence", test_job3.getCoreCompetency().getValue());
+
+        assertTrue(test_job3 instanceof Job);
+        assertTrue(test_job3.getEmployer() instanceof Employer);
+        assertTrue(test_job3.getLocation() instanceof Location);
+        assertTrue(test_job3.getPositionType() instanceof PositionType);
+        assertTrue(test_job3.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job test_job4 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+        Job test_job5 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+        assertFalse(test_job4.equals(test_job5));
+    }
+
+
 }
